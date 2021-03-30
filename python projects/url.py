1d062e367134c63736607beac947ac7aca5becd1
MWalega <67165630+MWalega@users.noreply.github.com>
2021-03-30 14:42:33 +0200

    Delete url.py

--- python projects/url.py
+++
@@ -1,6 +0,0 @@
-import urllib.request
-import shutil
-
-# Download the file from `url` and save it locally under `file_name`:
-with urllib.request.urlopen('https://upload.wikimedia.org/wikipedia/commons/3/37/African_Bush_Elephant.jpg') as response, open('plik2', 'wb') as out_file:
-    shutil.copyfileobj(response, out_file)
